1736381e19c97de51871d8e12611927d2c7167ef
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-20 14:01:53 -0400

    styled docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,26 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Guzzle theme
+extensions = [
+    'guzzle_sphinx_theme'
+]
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+html_sidebars = {'**': ['logo-text.html', 'globaltoc.html', 'searchbox.html']}
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "SeoBakery",
+}
