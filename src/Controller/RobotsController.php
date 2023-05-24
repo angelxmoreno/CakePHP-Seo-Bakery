6887731ad2536e643f8b784286293db7859d5bd2
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-24 14:50:55 -0400

    created RobotsController

---
+++ src/Controller/RobotsController.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace SeoBakery\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Response;
+use Cake\Utility\Hash;
+use SeoBakery\SeoBakeryPlugin;
+
+/**
+ * Robots Controller
+ *
+ */
+class RobotsController extends AppController
+{
+    public function display(): Response
+    {
+        $output = [];
+        $config = Configure::read(SeoBakeryPlugin::NAME . '.robotRules', []);
+        foreach ($config as $agent => $rules) {
+            $disallows = Hash::get($rules, 'disallow', []);
+            $allows = Hash::get($rules, 'allow', []);
+            $output[] = sprintf('User-agent: %s', $agent);
+            foreach ($disallows as $path) {
+                $output[] = sprintf('Disallow: %s', $path);
+            }
+            foreach ($allows as $path) {
+                $output[] = sprintf('Allow: %s', $path);
+            }
+            $output[] = "";
+        }
+//        $output[] = sprintf('Sitemap: %s', Router::url(Configure::read('Seo.paths.sitemapIndex'), true));
+
+        return $this->getResponse()->withType('text')->withStringBody(implode("\n", $output));
+    }
+}
---
+++ tests/TestCase/Controller/RobotsControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace SeoBakery\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * SeoBakery\Controller\RobotsController Test Case
+ *
+ * @uses \SeoBakery\Controller\RobotsController
+ */
+class RobotsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array<string>
+     */
+    protected $fixtures = [
+        'plugin.SeoBakery.Robots',
+    ];
+}
